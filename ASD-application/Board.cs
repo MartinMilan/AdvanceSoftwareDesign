5dbf8b1ab9cbc15f8a0d2aab0bcfa4449d37ba4b
Morten <morten@astrup.dev>
2021-12-14 16:38:11 +0100

    Something

---
+++ ASD-application/Board.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Board : IJobObserver
+{
+    private Category jobCategory;
+    private Job[] jobList;
+
+    public Board()
+    {
+
+    }
+
+    public void Update(Job job, Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void UpdateJobInfo()
+    {
+
+    }
+
+
+
+}
